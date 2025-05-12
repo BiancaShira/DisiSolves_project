7734b33af77be6db08ce63d7861821964d19bb21
BiancaShira <wanjirubianca97@gmail.com>
2025-05-12 16:08:21 +0300

    added landing page

--- app.py
+++ app.py
@@ -28,12 +28,26 @@ def index():
     problems = Problems.query.all()
     return render_template("index.html" , problems = problems)
 
-@app.route("/disisolves/admin")
+@app.route("/problems/<id>", methods =["GET", "POST"])
+def problem_details(id):
+    problem = Problems.query.filter_by(id = id).first_or_404()
+    return render_template("problem_detail.html" , problem=problem)
+
+@app.route("/admin/problems/<id>/edit")
+def update_problem():
+    return render_template("admin/edit_problem.html")
+
+@app.route("/problems")
+def problems():
+    problems= Problems.query.all()
+    return render_template("problems.html", problems = problems)
+
+@app.route("/admin")
 def admin():
     return render_template("./admin/admin.html")
 
 
-@app.route ("/disisolves/admin/register", methods =["GET" , "POST"])
+@app.route ("/admin/register", methods =["GET" , "POST"])
 def register():
     if request.method =="POST":    
         username=request.form.get('username')
@@ -57,7 +71,7 @@ def register():
     return render_template("./admin/register.html")
     
 
-@app.route("/disisolves/admin/login", methods=["GET", "POST"])
+@app.route("/admin/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
@@ -68,7 +82,7 @@ def login():
         check_password_hash = User.query.filter_by(password=password).first()
 
 
-        if not user or not check_password_hash:
+        if not user and not check_password_hash:
             flash("Please check your login details and try again.")
             return redirect(url_for("login"))
 
@@ -78,32 +92,37 @@ def login():
     return render_template("./admin/login.html")
 
 
-@app.route("/disisolves/admin/dashboard")
+@app.route("/admin/dashboard")
 @login_required
 def admin_dashboard():
     return render_template("./admin/admin_dashboard.html", username = current_user.username)
 
-@app.route("/disisolves/admin/problems/post" , methods=["GET" , "POST"])
-def create_problems():
+@app.route("/admin/problems/post" , methods=["GET" , "POST"])
+def post_problems():
     if request.method == "POST":
-        title = request.form.get('title')
-        description = request.form.get('description')
-        software_types = request.form.get('software_types') #flash_messages = [Tile Required , description]
+        problem_title = request.form.get('problem_title')
+        problem_description = request.form.get('problem_description')
+        software_types = request.form.get('software_types') #flash_messages = [Title Required , description]
+        solution = request.form.get('solution')
         has_error = False
-        if not title:
+        if not problem_title:
             flash("Title required" , category="error")
             has_error = True
-        if not description:
+        if not problem_description:
              flash("Description required" , category="error")
              has_error = True
         if not software_types:
             flash("Software Types Required" , category="error")
             has_error = True
+        if not solution:
+            flash("Solution is Required" , category="error")
+            has_error = True
         if not has_error:
             db.session.add(Problems(
-                title =  title,
-                description = description,
+                problem_title =  problem_title,
+                problem_description = problem_description,
                 software_types = software_types,
+                solution = solution,
                 posted_by = "Bianca"
             ))
 
@@ -112,24 +131,12 @@ def create_problems():
             return redirect("/")
     return render_template("admin/post_problems.html")
 
-
-@app.route("/disisolves/problems/<id>")
-def problem_details(id):
-    problem = Problems.query.filter_by(id = id).first_or_404()
-    return render_template("problem_detail.html" , problem=problem)
-
-
-
-@app.route("/disisolves/admin/problems/<id>/edit")
-def update_problem():
-    return render_template("admin/update_problem.html")
-
-
 class Problems(db.Model):
     id = db.Column(db.Integer , primary_key=True)
-    title=db.Column(db.Text , nullable = False)
-    description =db.Column(db.Text , nullable = False)
+    problem_title=db.Column(db.Text , nullable = False)
+    problem_description =db.Column(db.Text , nullable = False)
     software_types =db.Column(db.Enum(Software_types) , nullable = False)
+    solution= db.Column(db.Text, nullable =False)
     posted_by =db.Column(db.Text , nullable = False)
     posted_at =db.Column(db.DateTime(), index=True, default=datetime.now)
 
